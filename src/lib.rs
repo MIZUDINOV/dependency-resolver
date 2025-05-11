fb2e6e07cc58e3320c51681997a191249b4f9e3f
Mukhamed Izudinov <mui.xeon@gmail.com>
2025-05-12 00:17:46 +0300

    feat(lib): impl main logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub mod registry;
+
+use semver::Version;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use thiserror::Error;
+
+// Ошибки библиотеки
+#[derive(Debug, Error)]
+pub enum ResolverError {
+    #[error("Version not found: {0}")]
+    VersionNotFound(String),
+    #[error("No solution found")]
+    NoSolution,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// Описание пакета (упрощённый аналог package.json)
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Package {
+    pub name: String,
+    pub version: Version,
+    pub dependencies: HashMap<String, String>, // Имя пакета → SemVer-строка ("^1.0.0")
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+// Результат разрешения
+#[derive(Debug, Serialize)]
+pub struct ResolutionResult {
+    pub packages: HashMap<String, Version>, // Имя пакета → точная версия
 }
